2f0a28b034f58b029ff1eeb81bf0d59bf6f5df74
mwanek <89407740+mwanek@users.noreply.github.com>
2022-12-01 20:01:15 -0800

    Create puzzle.py

---
+++ mwanek/01/puzzle.py
@@ -0,0 +1,33 @@
+#!python
+""" AOC 2022: Day 1 """
+import pathlib
+
+def parse(puzzle_input: str) -> list[int]:
+    """ Parse input """
+    calories_carried_per_elf = []
+    for single_elf_data in puzzle_input.split('\n\n'):
+        meals = single_elf_data.split('\n')
+        total_calories = sum(map(int, meals))
+        calories_carried_per_elf.append(total_calories)
+    return sorted(calories_carried_per_elf)
+
+def part1(sorted_calories_per_elf: list) -> int:
+    """ Solve part 1 """
+    return sorted_calories_per_elf[-1]
+
+def part2(sorted_calories_per_elf: list) -> int:
+    """ Solve part 2 """
+    return sum(sorted_calories_per_elf[-3:])
+
+def solve(puzzle_input: str) -> tuple:
+    """ Parse, solve, return solutions """
+    data = parse(puzzle_input)
+    solution1 = part1(data)
+    solution2 = part2(data)
+    return solution1, solution2
+
+if __name__ == "__main__":
+    puzzle_input = pathlib.Path("input.txt").read_text(encoding='utf-8').strip()
+    answer1, answer2 = solve(puzzle_input)
+    print(f"Part 1: {answer1}")
+    print(f"Part 2: {answer2}")
