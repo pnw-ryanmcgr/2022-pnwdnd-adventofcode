82d466e92ba490a27108f622b63d35dd556b5e4d
Ryan McGregor <ryanmcgr@uw.edu>
2022-12-03 01:59:08 -0800

    Challenge Day3/B complete

--- pnw-ryanmcgr/day03/day03.py
+++ pnw-ryanmcgr/day03/day03.py
@@ -19,7 +19,7 @@ VALID_ITEMS = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
 class Rucksack():
     """Basic object to handle a single rucksack"""
-    def __init__(self,rucksack_line):
+    def __init__(self, rucksack_line):
         """
         Initializer
         Input: rucksack_line (str)
@@ -32,24 +32,51 @@ class Rucksack():
     @property
     def shared_items(self):
         """Provide the items shared between both compartments"""
-        items = list(set([x for x in self.compartment_1 if x in self.compartment_2]))
-        value = [x[0] + 1 for x in enumerate(list(VALID_ITEMS)) if x[1] in items ]
+        items = list({x for x in self.compartment_1 if x in self.compartment_2})
+        value = [x[0] + 1 for x in enumerate(list(VALID_ITEMS)) if x[1] in items]
         return [(items[x], value[x]) for x in range(len(items))]
+
+class ElfGroup():
+    """Basic object to handle a group of elves"""
+    def __init__(self, group_members):
+        """
+        Initializer
+        Input: group_members (list(str))
+        Output: none
+        """
+        self.members = []
+        for _member in group_members:
+            self.members.append(Rucksack(_member))
+
+    @property
+    def badge(self):
+        """Interpret/provide the group's badge"""
+        badge = [x for x in self.members[0].rucksack_line
+                 if x in self.members[1].rucksack_line and
+                    x in self.members[2].rucksack_line]
+        badge_value = [x[0] + 1 for x in enumerate(list(VALID_ITEMS)) if x[1] in badge]
+        return (badge[0], badge_value[0])
 
 if __name__ == '__main__':
     data = get_data(day=3, year=2022).split('\n')
-    answer_a = 0
-    for line in data:
-        rucksack = Rucksack(line)
-        print('===========')
-        print(f'Whole Line: {rucksack.rucksack_line}')
-        print(f'Chunk A: {rucksack.compartment_1}')
-        print(f'Chunk B: {rucksack.compartment_2}')
-        print(f'Shared Items/ Value: {rucksack.shared_items}')
-        for item in rucksack.shared_items:
-            answer_a += item[1]
-    print(f'Total: {answer_a}')
-
+    all_elves = data.copy()
+    answer_a, answer_b = 0, 0
+    elf_groups = []
+    for group in [all_elves[i:i + 3] for i in range(0, len(all_elves), 3)]:
+        elf_groups.append(ElfGroup(group))
+    for group in elf_groups:
+        print('======================')
+        print(f'Group members: {[x.rucksack_line for x in group.members]}')
+        print(f'Badge: {group.badge}')
+        answer_b += group.badge[1]
+        for member in group.members:
+            print('===========')
+            print(f'Whole Line: {member.rucksack_line}')
+            print(f'Chunk A: {member.compartment_1}')
+            print(f'Chunk B: {member.compartment_2}')
+            print(f'Shared Items/ Value: {member.shared_items}')
+            for item in member.shared_items:
+                answer_a += item[1]
     ## Submit Answer
     # submit(answer_a, part="a", day=3, year=2022)
-    # submit(total_score_b, part="b", day=3, year=2022)
+    # submit(answer_b, part="b", day=3, year=2022)
