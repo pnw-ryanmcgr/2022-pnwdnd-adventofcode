9a84a1181a1fad40d2621161be38a5ab0b10fa31
Ryan McGregor <ryanmcgr@uw.edu>
2022-12-05 02:34:34 -0800

    Linted.

--- pnw-ryanmcgr/day05/day05.py
+++ pnw-ryanmcgr/day05/day05.py
@@ -4,9 +4,9 @@ AOC2022:Day05
 https://adventofcode.com/2022/day/5
 """
 
-from aocd import get_data, submit
 from pprint import pprint
 import copy
+from aocd import get_data, submit
 import yaml
 
 ## Data manually cleaned for sanity sake.
@@ -24,11 +24,11 @@ class ContainerStack:
     def move_container_sequential(self, command):
         """
         Move a set of containers from one stack to another, one at a time
-        Input: command [num_boxes, from_stack, to_stack]
+        Input: move_order [num_boxes, from_stack, to_stack]
         Output: updated self.stack
         """
-        for container in range(0, int(command[0])):
-            self.stacks[command[2] - 1].append(self.stacks[command[1] - 1].pop())
+        for _ in range(0, int(command[0])):
+            self.stacks[move_order[2] - 1].append(self.stacks[command[1] - 1].pop())
         return self.stacks
 
     def move_container_bundle(self, command):
@@ -38,26 +38,27 @@ class ContainerStack:
         Output: updated self.stack
         """
         quantity = -1 * command[0]
-        self.stacks[command[2] - 1] = self.stacks[command[2] - 1] + self.stacks[command[1] - 1][quantity:]
+        self.stacks[command[2] - 1] = (self.stacks[command[2] - 1] +
+                                          self.stacks[command[1] - 1][quantity:])
         del self.stacks[command[1] - 1][quantity:]
         return self.stacks
 
 if __name__ == '__main__':
-    with open('./stacks.yaml', 'r') as file:
+    with open('./stacks.yaml', 'r', encoding="utf-8") as file:
         data = yaml.safe_load(file)
 
     container_stack_a = ContainerStack(copy.deepcopy(data['stacks']))
     print('===== ANSWER A =====')
-    for command in data['commands']:
-        container_stack_a.move_container_sequential(command)
+    for move_order in data['move_orders']:
+        container_stack_a.move_container_sequential(move_order)
     answer_a = ''.join([x[-1] for x in container_stack_a.stacks])
     print(f'Answer A: {answer_a}')
     # submit(answer_a, part="a", day=5, year=2022)
 
     container_stack_b = ContainerStack(copy.deepcopy(data['stacks']))
     print('===== ANSWER B =====')
-    for command in data['commands']:
-        container_stack_b.move_container_bundle(command)
+    for move_order in data['move_orders']:
+        container_stack_b.move_container_bundle(move_order)
     answer_b = ''.join([x[-1] for x in container_stack_b.stacks])
     print(f'Answer B: {answer_b}')
     # submit(answer_b, part="b", day=5, year=2022)
