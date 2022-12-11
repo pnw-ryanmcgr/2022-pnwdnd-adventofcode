ceeb14a52850c6dcc36f4110ad3494c037be7f94
Ryan McGregor <ryanmcgr@uw.edu>
2022-12-11 02:37:27 -0800

    Part 2 completed, linted

--- pnw-ryanmcgr/day11/day11.py
+++ pnw-ryanmcgr/day11/day11.py
@@ -9,7 +9,6 @@ from pprint import pprint
 from aocd import get_data, submit
 import yaml
 import numpy
-from datetime import datetime
 
 class Monkeys():
     """Basic object to manage a group of monkeys"""
@@ -21,8 +20,9 @@ class Monkeys():
         Outpost: None
         """
         self.monkeys = []
-        for monkey in monkeys:
-            self.monkeys.append(Monkey(self, monkey, destress))
+        for _monkey in monkeys:
+            self.monkeys.append(Monkey(self, _monkey, destress))
+        self.gcm = numpy.prod([x.test for x in self.monkeys])
 
 class Monkey():
     """Basic object to represent a monkey"""
@@ -64,9 +64,9 @@ class Monkey():
             operand = self.operation['operand']
         match self.operation['operator']:
             case "multiply":
-                new_number = operand * number
+                new_number = int((operand * number) % self.monkey_group.gcm)
             case "add":
-                new_number = operand + number
+                new_number = int((operand + number) % self.monkey_group.gcm)
 
         # Apply De-stress (Part A)
         if self.destress:
@@ -89,32 +89,20 @@ if __name__ == '__main__':
     # Answer A
     group_of_monkeys_a = Monkeys(data, destress=True)
     for _ in range(0, 20):
-        for monkey in group_of_monkeys_a.monkeys:
-            monkey.loop()
-    answer_a = numpy.prod(sorted([monkey.inspections for monkey in group_of_monkeys_a.monkeys])[-2:])
+        for _monkey in group_of_monkeys_a.monkeys:
+            _monkey.loop()
+    answer_a = numpy.prod(sorted(
+        [monkey.inspections for monkey in group_of_monkeys_a.monkeys])[-2:])
     print(f'Answer_A: {answer_a}')
     # submit(answer_a, part="a", day=11, year=2022)
 
     # Answer B
-    startTime = datetime.now()
-    timeLast = startTime
-    print(f'Start Time: {startTime}')
     group_of_monkeys_b = Monkeys(data, destress=False)
     for i in range(0, 10000):
-        if i % 10 == 0:
-            timeNow = datetime.now()
-            print('=============================')
-            print(f'Interation: {i}')
-            print(f'Last 10 interations time: {timeNow - startTime}')
-            print(f'Time since start: {timeNow - timeLast}')
-            print(f'Current Monkeys:')
-            pprint([x.inventory for x in group_of_monkeys_b.monkeys])
-            timeLast = timeNow
-        for monkey in group_of_monkeys_b.monkeys:
-            monkey.loop()
-    answer_b = numpy.prod(sorted([monkey.inspections for monkey in group_of_monkeys_b.monkeys])[-2:])
+        for _monkey in group_of_monkeys_b.monkeys:
+            _monkey.loop()
+    top_monkeys = sorted(
+        [monkey.inspections for monkey in group_of_monkeys_b.monkeys])[-2:]
+    answer_b = top_monkeys[0] * top_monkeys[1]
     print(f'Answer_b: {answer_b}')
-
-    # print(f'Answer_B: {answer_b}')
-
-    # submit(answer_b, part="b", day=7, year=2022)
+    # submit(answer_b, part="b", day=11, year=2022)
