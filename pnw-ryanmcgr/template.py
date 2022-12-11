3a2b86d74839362a95aecf04220dd0a8d7dc6e31
Ryan McGregor <ryanmcgr@uw.edu>
2022-12-11 14:37:48 -0800

    Adding template for individual days

---
+++ pnw-ryanmcgr/template.py
@@ -0,0 +1,22 @@
+
+"""
+Ryan McGregor, XXDec2022
+AOC2022:DayXX
+https://adventofcode.com/2022/day/XX
+"""
+
+from pprint import pprint
+from aocd import get_data, submit
+
+TEST_DATA = """
+"""
+
+if __name__ == '__main__':
+    data = [x for x in TEST_DATA.split('\n') if x]
+    # data = get_data(day=XX, year=2022)
+
+    # print(f'Answer_A: {answer_a}')
+    # print(f'Answer_B: {answer_b}')
+
+    # submit(answer_a, part="a", day=XX, year=2022)
+    # submit(answer_b, part="b", day=XX, year=2022)
